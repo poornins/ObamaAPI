c77ed65cd9f5b31b6ba109e964400572c10e22f2
poornins <poorninadeema96@gmail.com>
2022-10-22 00:52:07 +0530

    Order Mapping

--- src/main/java/com/obamaapi/controller/OrderController.java
+++ src/main/java/com/obamaapi/controller/OrderController.java
@@ -1,7 +1,10 @@
 package com.obamaapi.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/order")
 public class OrderController {
+
 }
